01e7dc488464105f002a5def887da349952ad2a1
phuonglevu3112@gmail.com <phuonglevu3112@gmail.com>
2023-02-02 22:28:58 +0800

    created set-up files

---
+++ setup.py
@@ -0,0 +1,10 @@
+# from setuptools import setup
+
+# if __name__ == '__main__':
+#     setup()
+
+from setuptools import setup, find_packages
+
+setup(name='melhousing', 
+    version='1.0', 
+    packages=find_packages(include=['melhousing']))
